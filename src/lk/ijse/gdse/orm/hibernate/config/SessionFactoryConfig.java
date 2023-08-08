c1709d4a55267cab4b295b8f62fbf7004e9dc6b2
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-08-08 21:34:17 +0530

    create Home Controller

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,59 +1,59 @@
-package lk.ijse.gdse.orm.hibernate.config;
-
-import lk.ijse.gdse.orm.hibernate.entity.Customer;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
-import javax.jms.Session;
-import javax.security.auth.login.Configuration;
-
-/**
- * This is the session factory configuration class
- */
-public class SessionFactoryConfig {
-
-    private static SessionFactory sessionFactory;
-
-    private static SessionFactoryConfig factoryConfig;
-
-    /**
-     * Defines default constructor as private
-     * to avoid object creation of this class from outside
-     */
-    private SessionFactoryConfig() {
-
-    }
-
-    static {
-        sessionFactory = new Configuration()
-                .configure()
-                .addAnnotatedClass(Customer.class)
-                .buildSessionFactory();
-
-    }
-
-    /**
-     * @return lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig
-     * Singleton the class to avoid object re-creation
-     */
-    public static SessionFactoryConfig getInstance() {
-        return (null == factoryConfig)
-                ? factoryConfig = new SessionFactoryConfig()
-                : factoryConfig;
-    }
-
-    /**
-     * @return org.hibernate.Session
-     * Returns Hibernate session whenever this method is called
-     * by following the steps of Native Bootstrapping
-     */
-    public Session getSession() {
-        return sessionFactory.openSession();
-    }
-}
+//package lk.ijse.gdse.orm.hibernate.config;
+//
+//import lk.ijse.gdse.orm.hibernate.entity.Customer;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+//
+//import javax.jms.Session;
+//import javax.security.auth.login.Configuration;
+//
+///**
+// * This is the session factory configuration class
+// */
+//public class SessionFactoryConfig {
+//
+//    private static SessionFactory sessionFactory;
+//
+//    private static SessionFactoryConfig factoryConfig;
+//
+//    /**
+//     * Defines default constructor as private
+//     * to avoid object creation of this class from outside
+//     */
+//    private SessionFactoryConfig() {
+//
+//    }
+//
+//    static {
+//        sessionFactory = new Configuration()
+//                .configure()
+//                .addAnnotatedClass(Customer.class)
+//                .buildSessionFactory();
+//
+//    }
+//
+//    /**
+//     * @return lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig
+//     * Singleton the class to avoid object re-creation
+//     */
+//    public static SessionFactoryConfig getInstance() {
+//        return (null == factoryConfig)
+//                ? factoryConfig = new SessionFactoryConfig()
+//                : factoryConfig;
+//    }
+//
+//    /**
+//     * @return org.hibernate.Session
+//     * Returns Hibernate session whenever this method is called
+//     * by following the steps of Native Bootstrapping
+//     */
+//    public Session getSession() {
+//        return sessionFactory.openSession();
+//    }
+//}
--- src/lk/ijse/gdse/orm/hibernate/controller/CustomerController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/CustomerController.java
@@ -1,4 +1,47 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class CustomerController {
+
+    @FXML
+    private ImageView imgCustomer;
+
+    @FXML
+    private ImageView imgItem;
+
+    @FXML
+    private ImageView imgOrder;
+
+    @FXML
+    private ImageView imgViewOrders;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label lblMenu;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void navigate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseEnterAnimation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseExitAnimation(MouseEvent event) {
+
+    }
+
 }
